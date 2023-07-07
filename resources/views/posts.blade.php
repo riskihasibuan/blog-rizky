51cf89883e1f67d9be3852f6b069ec6d933cb0eb
riskihasibuan <riskihasibuan2808@gmail.com>
2023-07-07 15:29:18 +0700

    update

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>WPU Blog | Posts</title>
+</head>
+<body>
+<h1>Halaman Posts</h1>
+</body>
+</html
