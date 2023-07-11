008ccb9b9b526688736a0946cb231063862d666d
mzaedan <zaedanm108@gmail.com>
2023-07-11 14:53:08 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 @foreach ($posts as $post)
 	<article class="mb-5">
 	<h2>
-		<a href="{{ url(/posts/{{ $post["slug"] }}") }}">
+		<a href="{{ url('/posts/'.$post['slug']) }}">{{ $post["title"] }}</a>
 	</h2>
 	<h5>by:{{ $post["author"] }}</h5>
 	<p>{{ $post["body"] }}</p>
