cdf682692b0622b04f11db549db1501609b5d837
riskihasibuan <riskihasibuan2808@gmail.com>
2023-07-26 14:10:41 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,29 +5,28 @@
 
 @if ($posts->count())
 <div class="card mb-3">
-	<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
-	<div class="card-body text-center">
-		<h3 class="card-title"><a href="{{ url('/posts/'.$posts[0]['id']) }}" class="text-decoration-none text-dark"> {{ $posts[0]->title }}</a></h3>
-		<p>
-			<small class="text-muted">
-				By. <a href="{{ url('authors/'.$posts[0]->user->username) }}" class="text-decoration-none">
-					@if(isset($posts[0]->user))
-					{{ $posts[0]->user->name }}
-					@endif
-				</a> in
-				<a href="{{ url('/categories/'.$posts[0]->category->slug) }}" class="text-decoration-none">
-					@if(isset($posts[0]->category))
-					{{ $posts[0]->category->name }}
-					@endif
-				</a>
-				@if(isset($posts[0]->created_at))
-				{{ $posts[0]->created_at->diffForHumans() }}
-				@endif
-			</small>
-		</p>
-		<p>{{ $posts[0]["excerpt"] }}</p>
+	<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+				<div class="card-body text center">
+					<h5 class="card-title">{{ $post->title }}</h5>
+					<p>
+						<small class="text-muted">
+							By. <a href="{{ url('authors/'.$posts[0]->user->username) }}" class="text-decoration-none">
+								@if(isset($posts[0]->user))
+								{{ $posts[0]->user->name }}
+								@endif
+							</a> in
+							<a href="{{ url('/categories/'.$posts[0]->category->slug) }}" class="text-decoration-none">
+								@if(isset($posts[0]->category))
+								{{ $posts[0]->category->name }}
+								@endif
+							</a>
+							@if(isset($posts[0]->created_at))
+							{{ $posts[0]->created_at->diffForHumans() }}
+							@endif
+						</small>
+					</p>
 
-		<a href="{{ url('/posts/'.$posts[0]['id']) }}" class=" text-decoration-none btn btn-primary">Read more</a>
+					<p>{{ $posts[0]["excerpt"] }}</p>
 
 	</div>
 </div>
@@ -38,23 +37,23 @@
 	<div class="row">
 		@foreach ($posts->skip(1) as $post)
 
-		<div class="col-md-4">
+		<div class="col-md-4 mb-3">
 			<div class="card">
-				<img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-				<div class="card-body text center">
-					<h5 class="card-title">{{ $post->title }}</h5>
-					<p>
-						@foreach($posts as $post)
-						<small class="text-muted">
-							By. <a href="{{ url('authors/'.$post->user->username) }}" class="text-decoration-none">
-								{{ $post->user->name }}
-							</a>
-							{{ $post->created_at->diffForHumans() }}
-						</small>
-						@endforeach
-					</p>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<a href="#" class="btn btn-primary">Read more</a>
+				<img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}"  class="card-img-top" alt="{{ $post->category->name }}">
+	<div class="card-body">
+		<h5 class="card-title">{{ $post->title }}</h5>
+		<p>
+			<small class="text-muted">
+				By. <a href="{{ url('authors/'.$post->author->username) }}" class="text-decoration-none">
+				{{ $post->author->name }}	
+				</a>{{
+					$post->created_at->diffForHumans()
+				}}
+			</small>
+		</p>
+		<p>{{ $post->excerpt }}</p>
+
+		<a href="{{ url('/posts/'.$post->slug) }}" class=" text-decoration-none btn btn-primary">Read more</a>
 				</div>
 			</div>
 		</div>
