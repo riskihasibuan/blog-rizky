3c350f491151652b4cf53b2adeccfefcf7708280
mzaedan <zaedanm108@gmail.com>
2023-07-27 08:54:03 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,28 +5,28 @@
 
 @if ($posts->count())
 <div class="card mb-3">
-	<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-				<div class="card-body text center">
-					<h5 class="card-title">{{ $post->title }}</h5>
-					<p>
-						<small class="text-muted">
-							By. <a href="{{ url('authors/'.$posts[0]->user->username) }}" class="text-decoration-none">
-								@if(isset($posts[0]->user))
-								{{ $posts[0]->user->name }}
-								@endif
-							</a> in
-							<a href="{{ url('/categories/'.$posts[0]->category->slug) }}" class="text-decoration-none">
-								@if(isset($posts[0]->category))
-								{{ $posts[0]->category->name }}
-								@endif
-							</a>
-							@if(isset($posts[0]->created_at))
-							{{ $posts[0]->created_at->diffForHumans() }}
-							@endif
-						</small>
-					</p>
+	<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]['category']['name'] }}" class="card-img-top" alt="{{ $posts[0]['category']['name'] }}">
+	<div class="card-body text center">
+		<h5 class="card-title">{{ $posts[0]['title'] }}</h5>
+		<p>
+			<small class="text-muted">
+				By. <a href="{{ url('authors/'.$posts[0]->user->username) }}" class="text-decoration-none">
+					@if(isset($posts[0]->user))
+					{{ $posts[0]->user->name }}
+					@endif
+				</a> in
+				<a href="{{ url('/categories/'.$posts[0]->category->slug) }}" class="text-decoration-none">
+					@if(isset($posts[0]->category))
+					{{ $posts[0]->category->name }}
+					@endif
+				</a>
+				@if(isset($posts[0]->created_at))
+				{{ $posts[0]->created_at->diffForHumans() }}
+				@endif
+			</small>
+		</p>
 
-					<p>{{ $posts[0]["excerpt"] }}</p>
+		<p>{{ $posts[0]["excerpt"] }}</p>
 
 	</div>
 </div>
@@ -39,21 +39,21 @@
 
 		<div class="col-md-4 mb-3">
 			<div class="card">
-				<img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}"  class="card-img-top" alt="{{ $post->category->name }}">
-	<div class="card-body">
-		<h5 class="card-title">{{ $post->title }}</h5>
-		<p>
-			<small class="text-muted">
-				By. <a href="{{ url('authors/'.$post->author->username) }}" class="text-decoration-none">
-				{{ $post->author->name }}	
-				</a>{{
+				<img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+				<div class="card-body">
+					<h5 class="card-title">{{ $post->title }}</h5>
+					<p>
+						<small class="text-muted">
+							By. <a href="{{ url('authors/'.$post->author->username) }}" class="text-decoration-none">
+								{{ $post->author->name }}
+							</a>{{
 					$post->created_at->diffForHumans()
 				}}
-			</small>
-		</p>
-		<p>{{ $post->excerpt }}</p>
+						</small>
+					</p>
+					<p>{{ $post->excerpt }}</p>
 
-		<a href="{{ url('/posts/'.$post->slug) }}" class=" text-decoration-none btn btn-primary">Read more</a>
+					<a href="{{ url('/posts/'.$post->slug) }}" class=" text-decoration-none btn btn-primary">Read more</a>
 				</div>
 			</div>
 		</div>
