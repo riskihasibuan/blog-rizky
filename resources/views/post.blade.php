096f03afb1f120d1fbd2983499347a82a4e481f5
riskihasibuan <riskihasibuan2808@gmail.com>
2023-08-18 14:53:07 +0700

    update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,8 @@
 
 @section('container')	
 		<h1 class="mb-5">{{ $post["title"] }}</h1>
-
-		<p>By.Riski Hasibuan in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
-
+		<a href="{{ url('/dashboard/posts') }}" class="btn btn-success"><i class="bi bi-arrow-left"></i> Back to Posts</a>
+		<br>
 		{!! $post->body !!}
-	
-		<a href="{{ url('/dashboard/posts') }}" class="btn btn-success"></span></a>
+		
 @endsection
