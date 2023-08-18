ea7d4f3026406efd2ebed6b627af1aa278032a09
mzaedan <zaedanm108@gmail.com>
2023-08-18 14:53:44 +0700

    update

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,84 +1,27 @@
 @extends('dashboard.layouts.main')
 
-@section('container')
-@extends('layouts.main')
-
-@section('container')
-<h1 class="mb-3 text-center">All Posts</h1>
-
-<div class="row justify-content-center mb-3">
-	<div class="col-md-6">
-		<form action="/posts">
-			<div class="input-group mb-3">
-				<input type="text" class="form-control" placeholder="Search.." name="search">
-				<button class="btn btn-danger" type="submit">Search</button>
-			</div>
-		</form>
-	</div>
-</div>
-
-@if ($posts->count())
-<div class="card mb-3">
-	<div class="row">
-		<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]['category']['name'] }}" class="card-img-top" alt="{{ $posts[0]['category']['name'] }}">
-		<div class="card-body text-center">
-			<h5 class="card-title">{{ $posts[0]['title'] }}</h5>
-			<p>
-				<small class="text-muted">
-					By. <a href="{{ url('authors/'.$posts[0]->user->username) }}" class="text-decoration-none">
-						@if(isset($posts[0]->user))
-						{{ $posts[0]->user->name }}
-						@endif
-					</a> in
-					<a href="{{ url('/categories/'.$posts[0]->category->slug) }}" class="text-decoration-none">
-						@if(isset($posts[0]->category))
-						{{ $posts[0]->category->name }}
-						@endif
-					</a>
-					@if(isset($posts[0]->created_at))
-					{{ $posts[0]->created_at->diffForHumans() }}
-					@endif
-				</small>
-			</p>
-
-			<p>{{ $posts[0]["excerpt"] }}</p>
-			<a href="" class=" text-decoration-none btn btn-primary">Read more</a>
-		</div>
-	</div>
-</div>
+@section('content')
 
 <div class="container">
-	<div class="row">
-		@foreach ($posts->skip(1) as $post)
-		<div class="col-md-4 mb-3">
-			<div class="card">
-				<div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0,0,0,0.7)"><a href="{{ url('/categories/'.$post->category->slug) }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-				<img src="https://source.unsplash.com/200x100?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-				<div class="card-body">
-					<h5 class="card-title">{{ $post->title }}</h5>
-					<p>
-						<small class="text-muted">
-							html
-							By. <a href="{{ url('authors/'. ($post->author->username ?? '')) }}" class="text-decoration-none">
-								{{ $post->author->name ?? '' }}
-							</a>{{ $post->created_at->diffForHumans() }}
-						</small>
-					</p>
-					<p class="card-text">{{ $post->excerpt }}</p>
-					<a href="{{ url('/posts/'.$post->slug) }}" class=" text-decoration-none btn btn-primary">Read more</a>
-				</div>
+	<div class="row my-3">
+		<div class="col-lg-8">
+
+			<h1 class="mb-3">{{ $post->title }}</h1>
+
+			<a href="{{ url('dashboard/posts') }}" class="btn btn-success"><span data-feather="arrow-left"></span>Back To My Posts</a>
+			<a href="{{ url('dashboard/posts/'.$post->slug.'/edit') }}" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
+			@if($post->image)
+			<div style="max-height: 350px;overflow:hidden;">
+				<img src="{{ asset('storage/'.$post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 			</div>
+			@else
+			<img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+			@endif
+			<article class="my-3 fs-5">
+				{!! $post->body !!}
+			</article>
 		</div>
-		@endforeach
 	</div>
 </div>
 
-
-@else
-<p class="text-center fs-4">No post found.</p>
-@endif
-
-{{ $posts->links() }}
-
-@endsection
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
-@section('container')	
-		<h1 class="mb-5">{{ $post["title"] }}</h1>
+@section('container')
+<h1 class="mb-5">{{ $post["title"] }}</h1>
 
-		<p>By.Riski Hasibuan in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
+<p>By.Riski Hasibuan in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
 
-		{!! $post->body !!}
-	
-		<a href="{{ url('/dashboard/posts') }}" class="btn btn-success"></span></a>
+{!! $post->body !!}
+
+<a href="{{ url('/dashboard/posts') }}" class="btn btn-success"></span></a>
 @endsection
